ba08fbe83b450d7dfbebc6b601fb4c72f6232146
milnay <126925768+milnay@users.noreply.github.com>
2024-10-11 18:53:21 +1100

    Add files via upload

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,9 @@
+var vg1 = "js/critical_mines_map.vg.json"
+vegaEmbed('#critical_mines_map', vg1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+
+var vg2 = "js/exploration_expenditure.vg.json"
+vegaEmbed('#exploration_expend_deposit_type', vg2).then(function(result){
+
+}).catch(console.error)
