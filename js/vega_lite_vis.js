bf8abe975b6fc184990cdd9e52804b37cd0dd472
milnay <126925768+milnay@users.noreply.github.com>
2024-10-16 23:52:55 +1100

    Update vega_lite_vis.js

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,9 +1,33 @@
-var vg1 = "js/critical_mines_map.vg.json"
-vegaEmbed('#critical_mines_map', vg1).then(function(result) {
+
+var opt = {
+  actions: false
+}
+//Critical mines map
+var vg1 = "https://raw.githubusercontent.com/milnay/FIT3179-DataViz2/refs/heads/main/js/critical_mines_map.vg.json"
+vegaEmbed('#critical_mines_map', vg1, {actions:false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
 
-var vg2 = "js/exploration_expenditure_deposit_type.vg.json"
-vegaEmbed('#exploration_expend_deposit_type', vg2).then(function(result){
+//Mineral production also by state
+var vg2 = "https://raw.githubusercontent.com/milnay/FIT3179-DataViz2/refs/heads/main/js/mineral_production_state.vg.json"
+vegaEmbed('#mineral_production', vg2, {actions:false}).then(function(result){
 
 }).catch(console.error)
+
+//Exploration Expenditure by deposit type
+var vg3 = "https://raw.githubusercontent.com/milnay/FIT3179-DataViz2/refs/heads/main/js/exploration_expenditure_deposit_type.vg.json"
+vegaEmbed('#exploration_expend_deposit_type', vg3, {actions:false}).then(function(result){
+
+}).catch(console.error)
+
+//Exploartion Expenditure
+var vg6 = "https://raw.githubusercontent.com/milnay/FIT3179-DataViz2/refs/heads/main/js/exploration_expenditure.vg.json"
+vegaEmbed('#exploration_expenditure', vg6, {actions:false}).then(function(result){
+
+}).catch(console.error)
+//Exports Flowmap
+var vg5 = "https://raw.githubusercontent.com/milnay/FIT3179-DataViz2/refs/heads/main/js/exports_flowmap.vg.json"
+vegaEmbed('#exports_flowmap', vg5, {actions:false}).then(function(result){
+
+}).catch(console.error)
+
